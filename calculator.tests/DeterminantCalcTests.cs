3d5eea6617f898296b2cdab741e1be737720bcfc
Vladimir <riuson@gmail.com>
2020-12-18 18:51:35 +0500

    Add test for signs

---
+++ calculator.tests/DeterminantCalcTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace calculator.tests
+{
+    internal class DeterminantCalcTests
+    {
+        [TestCase(0, 0, ExpectedResult = 1)]
+        [TestCase(1, 0, ExpectedResult = -1)]
+        [TestCase(2, 0, ExpectedResult = 1)]
+        [TestCase(3, 0, ExpectedResult = -1)]
+        [TestCase(0, 1, ExpectedResult = -1)]
+        [TestCase(1, 1, ExpectedResult = 1)]
+        [TestCase(2, 1, ExpectedResult = -1)]
+        [TestCase(3, 1, ExpectedResult = 1)]
+        [TestCase(0, 2, ExpectedResult = 1)]
+        [TestCase(1, 2, ExpectedResult = -1)]
+        [TestCase(2, 2, ExpectedResult = 1)]
+        [TestCase(3, 2, ExpectedResult = -1)]
+        [TestCase(0, 3, ExpectedResult = -1)]
+        [TestCase(1, 3, ExpectedResult = 1)]
+        [TestCase(2, 3, ExpectedResult = -1)]
+        [TestCase(3, 3, ExpectedResult = 1)]
+        public int CanGetSign(int x, int y)
+        {
+            // Arrange.
+            var calc = new DeterminantCalc();
+
+            // Act & Assert.
+            return calc.GetSign(x, y);
+        }
+    }
+}
---
+++ calculator/DeterminantCalc.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace calculator
+{
+    public class DeterminantCalc
+    {
+        public int GetSign(in int i, in int i1)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
